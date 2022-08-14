f6e089ffa008bb9a672298c5cc59bd393b3baa7c
Davi <fcodavibandeira@edu.unifor.br>
2022-08-14 00:12:48 -0300

    hit effect on enemy

--- scripts/enemy/template.gd
+++ scripts/enemy/template.gd
@@ -1,12 +1,16 @@
 extends Area2D
 class_name EnemyTemplate
 
+onready var animation: AnimationPlayer = get_node("Animation")
+
 var health: int
+var distance: float
 var direction: Vector2
 
 export(int) var speed
 export(int) var min_health
 export(int) var max_health
+export(int) var distance_threshold
 
 func _ready() -> void:
 	randomize()
@@ -15,6 +19,10 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	direction = global_position.direction_to(global_info.character.global_position) 
+	distance = global_position.distance_to(global_info.character.global_position)
+	if distance < distance_threshold:
+		return
+		
 	translate(direction * speed * delta)
 	
 	
@@ -22,3 +30,5 @@ func update_health(damage: int) -> void:
 	health -= damage
 	if health <= 0:
 		queue_free()
+		
+	animation.play("hit")
