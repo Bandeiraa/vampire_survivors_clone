9ce7aef85d592be6da72e91f957047b4a9ab1e57
Davi <fcodavibandeira@edu.unifor.br>
2022-08-14 20:19:48 -0300

    lava floor spell

---
+++ scripts/spell/spell/lava_floor.gd
@@ -0,0 +1,61 @@
+extends SpellTemplate
+class_name LavaFloor
+
+onready var attack_cooldown: Timer = get_node("AttackCooldown")
+
+onready var collision: CollisionShape2D = get_node("Collision")
+onready var sprite: Sprite = get_node("Texture")
+
+var hit_cooldown: float
+var enemies_in_range: Array
+
+var direction_list: Array = [
+	Vector2.UP,
+	Vector2.DOWN,
+	Vector2.LEFT,
+	Vector2.RIGHT
+]
+
+export(StreamTexture) var lava_floor
+
+func _ready() -> void:
+	direction = direction_list[randi() % direction_list.size()]
+	
+	
+func _physics_process(delta: float) -> void:
+	if can_move:
+		translate(delta * speed * direction)
+		
+	if can_rotate:
+		rotation_degrees += delta * rotation_speed
+		
+		
+func on_area_entered(area) -> void:
+	if area is EnemyTemplate:
+		area.update_health(damage)
+		enemies_in_range.append(area)
+		
+		
+func on_area_exited(area) -> void:
+	if not (area is EnemyTemplate):
+		return
+		
+	var index: int = enemies_in_range.find(area)
+	if index == -1:
+		return
+		
+	enemies_in_range.remove(index)
+	
+	
+func on_attack_cooldown_timeout() -> void:
+	for enemy in enemies_in_range:
+		enemy.update_health(damage)
+		
+		
+func on_throw_lifetime_timeout() -> void:
+	can_move = false
+	can_rotate = false
+	
+	collision.disabled = false
+	sprite.texture = lava_floor
+	attack_cooldown.start(hit_cooldown)
--- scripts/spell/spell/spell_template.gd
+++ scripts/spell/spell/spell_template.gd
@@ -7,6 +7,7 @@ var rotation_speed: int = 180
 
 var direction: Vector2
 
+export(bool) var can_kill = true
 export(bool) var can_move = true
 export(bool) var can_rotate = true
 
@@ -24,4 +25,5 @@ func on_area_entered(area) -> void:
 		
 		
 func on_screen_exited() -> void:
-	queue_free()
+	if can_kill:
+		queue_free()
